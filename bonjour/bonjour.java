370b90e4ec76dff2f91c4cff89e3395fb59fe9c6
theosamson <46024624+theosamson@users.noreply.github.com>
2020-05-06 12:15:07 +0200

    Add files via upload

---
+++ bonjour/bonjour.java
@@ -0,0 +1,10 @@
+/** Votre premier programme Java */
+class bonjour {
+
+    public static void main (String[] args){
+        String x;
+        x = "Theo";
+
+        System.out.println("Bonjour" + x + "!");
+    }
+}
